81083a32080238b1c70d5fd9c321040fd679ad06
Albert J. Wong <awong.dev@gmail.com>
2019-10-20 23:46:00 -0700

    Clean up html inteface. Add stats panel, fb config, etc

--- src/main/static/basic_controls.js
+++ src/main/static/basic_controls.js
@@ -34,18 +34,51 @@ const EspCxxControls = {};
     };
 
     postToUrl('/api/wificonfig', update);
-  }
+  };
 
   // Handles a reset request.
   const onResetSubmit = (event) => {
     event.preventDefault();
     postToUrl('/api/reset', {});
     // TODO(awong): Reload page after 5 seconds.
-  }
+  };
+
+  // Handles a Firebase config request.
+  const onConfigSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const data = {};
+    [...form.elements].forEach((input) => {
+      data[input.name] = input.value;
+    });
+    data['_namespace'] = form.name;
+    postToUrl('/api/config', data);
+    // TODO(awong): Reset
+  };
+
+  const updateStats = () => {
+    fetch("/api/stats")
+      .then(response => {
+              const stats_element = document.getElementById('stats');
+              while (stats_element.firstChild) {
+                stats_element.removeChild(stats_element.firstChild);
+              }
+              const stats = response.json();
+              // Iterate through stats and dump into list items.
+              for (let key in stats) {
+                const li = document.createElement('li');
+                li.textContent = `${key}: ${stats[key]}`;
+                stats_element.appendChild(li);
+              }
+      })
+      .catch(error => displayResult('Exception', error.message));
+  };
 
   //// Init code
   window.addEventListener('load', () => {
     document.forms.wificonfig.addEventListener('submit', onWifiConfigSubmit);
     document.forms.reset.addEventListener('submit', onResetSubmit);
+    document.forms.firebase.addEventListener('submit', onConfigSubmit);
+    setInterval(updateStats, 1000);
   });
 })();
